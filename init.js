09a1ab3107e55c102700ed39b473164167da97c2
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2022-10-31 10:16:19 -0700

    simplifying to step 4

--- init.js
+++ init.js
@@ -1,16 +1,19 @@
 import { createClient } from 'redis';
 
 import { fetchAuth } from './services/auth.js';
+import { fetchEvents } from './services/events.js';
 
 const { ER_USERNAME } = process.env;
 
-export default async () => {
-  const redisClient = createClient();
-  await redisClient.connect();
-  
-  const token = await fetchAuth();
+const redisClient = createClient();
+await redisClient.connect();
 
-  await redisClient.set(`token:${ER_USERNAME}`, JSON.stringify(token));
+const token = await fetchAuth();
 
-  return redisClient;
-};
+await redisClient.set(`token:${ER_USERNAME}`, JSON.stringify(token));
+
+const events = await fetchEvents(token.access_token);
+
+console.log('events', events?.data?.results);
+
+process.exit(0);
